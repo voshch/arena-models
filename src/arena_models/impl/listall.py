2afc65ae8936d55961a20936eca8c6fe8221e3d3
Vova Shcherbyna <dev@voshch.dev>
2026-06-11 16:55:59 +0200

    list with asset-specific annotation class

--- src/arena_models/impl/listall.py
+++ src/arena_models/impl/listall.py
@@ -1,12 +1,15 @@
 import os
 
-from arena_models.utils.Database import Database, Annotation
+from arena_models.utils.Database import Database
 
-from . import DATABASE_NAME, AssetType
+from . import DATABASE_NAME, Annotation, AssetType
+from arena_models.impl.build import DatabaseBuilder
 
 
 def list_database(database_path: str, asset_type: AssetType) -> list[Annotation]:
     db = Database(os.path.join(database_path, DATABASE_NAME))
     result = db.list_all(asset_type.value)
 
-    return [Annotation.from_metadata(dict(item)) for item in result['metadatas'] if item]
+    annotation_t = DatabaseBuilder.Builder(asset_type)._annotation_cls
+
+    return [annotation_t.from_metadata(dict(item)) for item in result['metadatas'] if item]
